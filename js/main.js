451a3e57cfc5d2e2276d9f8afe5f4299eb21b577
Ahmadjon-Frontend <102577930+Ahmadjon-Frontend@users.noreply.github.com>
2023-04-16 15:58:33 +0300

    cs

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+var  a = 10
+var  b = 5  
+console.log(
+  (a>b||b==a)&&(b<=b && a<a)
+);
+console.log( userName);
+console.log( name;)
+console.log()
+var userName = [ user1]
+let name = 55
+const names =66
